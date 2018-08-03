d14a2bee679276f7229173a2c7819556df2c2f02
yingwei <457958630@qq.com>
2018-08-03 20:51:59 +0800

    搭建项目的基本结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入css
+import "./assets/statics/site/css/style.css";
+
 Vue.config.productionTip = false
 
 new Vue({
+  el:"#app",
   render: h => h(App)
-}).$mount('#app')
+})
